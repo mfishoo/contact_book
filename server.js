72ed640f5a8526bc0a3d3c9adf4e86d0225300ff
Mei <m_fishoo@hotmail.com>
2020-07-29 00:59:39 +0800

    Express server setup with initial routes

---
+++ server.js
@@ -0,0 +1,18 @@
+// initialize the server
+const express = require('express');
+
+// initialize a server object
+const app = express();
+
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the contact book API...' })
+);
+
+// Define Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
