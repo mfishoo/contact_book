2e152610e771ce6d063d5f45546d0a34950ae665
Mei <m_fishoo@hotmail.com>
2020-07-30 20:46:32 +0800

    prepared for production

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const express = require('express');
 // db
 const connectDB = require('./config/db');
 
+const path = require('path');
+
 // initialize a server object
 const app = express();
 
@@ -13,14 +15,24 @@ connectDB();
 // Init Middleware, request can accept body data
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) =>
-  res.json({ msg: 'Welcome to the contact book API...' })
-);
+// app.get('/', (req, res) =>
+//   res.json({ msg: 'Welcome to the contact book API...' })
+// );
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets(React) in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
